44e77fa2296a48721bcfce519fca1a411056a6d3
Jake Torrance <jaket1234@hotmail.com>
2016-08-17 16:55:33 -0400

    Added ValidatorResource class - validates json to check if required fields exist in the data.

--- bloodedguild_forums_api/api.py
+++ bloodedguild_forums_api/api.py
@@ -1,33 +1,89 @@
-from flask import Flask, jsonify, request, make_response, abort
-from flask_restful import Resource, Api
+from flask import Flask, jsonify, request, make_response
+from flask_restful import Resource, Api, abort
 import sqlite3 as sql
 import sys
 import hashlib
+import psycopg2
+
+DB_NAME = "bloodedguild"
+USER = "***REMOVED***"
+PASSWORD = "***REMOVED***"
 
 app = Flask(__name__)
 api = Api(app)
+
+class ValidatorResource(Resource):
+    def __init__(self, required_fields=[]):
+        super().__init__()
+        self.required_fields = required_fields
+
+    def validate_json(self, json_data):
+        missing_fields = []
+        for field in self.required_fields:
+            if(field not in json_data.keys()):
+                missing_fields.append(field)
+        if(missing_fields):
+            error_string = "The JSON data was missing the following fields:"
+            for field in missing_fields:
+                error_string += field+","
+                error_string = error_string[:-1] #trim last comma
+                abort(400, message=error_string)
+        else:
+            return json_data
+
+class DatabaseStringConstructor():
+    def __init__(self, db_name, user, password):
+        self.db_name = db_name
+        self.user = user
+        self.password = password
+
+    def __str__(self):
+        conn_string = "host='localhost' "
+        conn_string +="dbname='"+self.db_name+"' "
+        conn_string +="user='"+self.user+"' "
+        conn_string +="password='"+self.password+"' "
+        return conn_string
+
+
+class DatabaseConnector():
+    def __init__(self):
+        self.db_string_constructor = DatabaseStringConstructor(
+            DB_NAME,
+            USER,
+            PASSWORD
+        )
+
+    def get_conn_string(self):
+        return str(db_string_constructor)
+
+    def make_connection(self):
+        connection = psycopg2.connect(self.get_conn_string())
+        return connection.cursor()
+
 
 class DefaultLocation(Resource):
     def get(self):
         return {'info': 'Bloodedguild forums api. Do not touch! wow!'}
 
 
-class Forums(Resource):
+class Forums(ValidatorResource):
+
     def get(self):
         return {
             'type': 'forums',
         }
 
     def put(self):
-        print("create some thread")
+        json_data = self.validate_json(request.get_json())
         return {
+            'title': json_data["title"],
             'type': 'thread',
             'id': 'SOME/FUCKING/ID',
             'status': 'created'
         }
 
 
-class ForumsThread(Resource):
+class ForumsThread(ValidatorResource):
     def get(self, thread_id):
         return {
             'type': 'thread',
@@ -41,7 +97,7 @@ class ForumsThread(Resource):
             'status': 'created'
         }
 
-class ForumsPost(Resource):
+class ForumsPost(ValidatorResource):
     def get(self, thread_id, post_id):
         return {
             'type': 'post',
@@ -50,7 +106,12 @@ class ForumsPost(Resource):
         }
 
 api.add_resource(DefaultLocation, '/')
-api.add_resource(Forums, '/forums')
+forums_required_fields = ([["title","category_id", "subcategory_id"]])
+api.add_resource(
+    Forums,
+    '/forums',
+    resource_class_args=forums_required_fields
+)
 api.add_resource(ForumsThread, '/forums/<int:thread_id>')
 api.add_resource(ForumsPost, '/forums/<int:thread_id>/<int:post_id>')
 
